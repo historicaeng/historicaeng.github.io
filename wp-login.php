07d578b30a4e95e584c9dca3b6a23887e55bc392
felipecastrotc <felipecarvalho50@gmail.com>
2017-10-17 01:18:42 -0200

    update-2017-10-17

--- wp-login.php
+++ wp-login.php
@@ -8,22 +8,12 @@
 	<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Histórica Engenharia &lsaquo; Log In</title>
-<<<<<<< HEAD
-	<link rel='dns-prefetch' href='//henas01f.myqnapcloud.com' />
-<link rel='dns-prefetch' href='//s.w.org' />
-<script type='text/javascript' src='https://henas01f.myqnapcloud.com/wp-admin/load-scripts.php?c=1&amp;load%5B%5D=jquery-core,jquery-migrate&amp;ver=4.8.2'></script>
-<script type='text/javascript' src='https://henas01f.myqnapcloud.com/wp-content/plugins/jetpack/modules/sso/jetpack-sso-login.js?ver=5.4'></script>
-<link rel='stylesheet' href='https://henas01f.myqnapcloud.com/wp-admin/load-styles.php?c=1&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.8.2' type='text/css' media='all' />
-<link rel='stylesheet' id='genericons-css'  href='https://henas01f.myqnapcloud.com/wp-content/plugins/menu-icons/includes/library/icon-picker/css/types/genericons.min.css?ver=3.4' type='text/css' media='all' />
-<link rel='stylesheet' id='jetpack-sso-login-css'  href='https://henas01f.myqnapcloud.com/wp-content/plugins/jetpack/modules/sso/jetpack-sso-login.css?ver=5.4' type='text/css' media='all' />
-=======
 	<link rel='dns-prefetch' href='//s.w.org' />
-<script type='text/javascript' src='https://historicaeng.github.io/wp-admin/load-scripts.php?c=1&amp;load%5B%5D=jquery-core,jquery-migrate&amp;ver=4.8.2'></script>
-<script type='text/javascript' src='https://historicaeng.github.io/wp-content/plugins/jetpack/modules/sso/jetpack-sso-login.js?ver=5.4'></script>
-<link rel='stylesheet' href='https://historicaeng.github.io/wp-admin/load-styles.php?c=1&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.8.2' type='text/css' media='all' />
-<link rel='stylesheet' id='genericons-css'  href='https://historicaeng.github.io/wp-content/plugins/menu-icons/includes/library/icon-picker/css/types/genericons.min.css?ver=3.4' type='text/css' media='all' />
-<link rel='stylesheet' id='jetpack-sso-login-css'  href='https://historicaeng.github.io/wp-content/plugins/jetpack/modules/sso/jetpack-sso-login.css?ver=5.4' type='text/css' media='all' />
->>>>>>> parent of 406cb7f... update-2017-10-17
+<script type='text/javascript' src='https://www.historicaengenharia.com/wp-admin/load-scripts.php?c=1&amp;load%5B%5D=jquery-core,jquery-migrate&amp;ver=4.8.2'></script>
+<script type='text/javascript' src='https://www.historicaengenharia.com/wp-content/plugins/jetpack/modules/sso/jetpack-sso-login.js?ver=5.4'></script>
+<link rel='stylesheet' href='https://www.historicaengenharia.com/wp-admin/load-styles.php?c=1&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.8.2' type='text/css' media='all' />
+<link rel='stylesheet' id='genericons-css'  href='https://www.historicaengenharia.com/wp-content/plugins/menu-icons/includes/library/icon-picker/css/types/genericons.min.css?ver=3.4' type='text/css' media='all' />
+<link rel='stylesheet' id='jetpack-sso-login-css'  href='https://www.historicaengenharia.com/wp-content/plugins/jetpack/modules/sso/jetpack-sso-login.css?ver=5.4' type='text/css' media='all' />
 			<style>
 				.jetpack-sso .message {
 					margin-top: 20px;
@@ -36,20 +26,16 @@
 			</style>
 		<meta name='robots' content='noindex,follow' />
 	<meta name="viewport" content="width=device-width" />
-	<link rel="icon" href="https://i0.wp.com/henas01f.myqnapcloud.com/wp-content/uploads/2017/07/favicon.png?fit=32%2C32&#038;ssl=1" sizes="32x32" />
-<link rel="icon" href="https://i0.wp.com/henas01f.myqnapcloud.com/wp-content/uploads/2017/07/favicon.png?fit=128%2C128&#038;ssl=1" sizes="192x192" />
-<link rel="apple-touch-icon-precomposed" href="https://i0.wp.com/henas01f.myqnapcloud.com/wp-content/uploads/2017/07/favicon.png?fit=128%2C128&#038;ssl=1" />
-<meta name="msapplication-TileImage" content="https://i0.wp.com/henas01f.myqnapcloud.com/wp-content/uploads/2017/07/favicon.png?fit=128%2C128&#038;ssl=1" />
+	<link rel="icon" href="https://i1.wp.com/www.historicaengenharia.com/wp-content/uploads/2017/07/favicon.png?fit=32%2C32&#038;ssl=1" sizes="32x32" />
+<link rel="icon" href="https://i1.wp.com/www.historicaengenharia.com/wp-content/uploads/2017/07/favicon.png?fit=128%2C128&#038;ssl=1" sizes="192x192" />
+<link rel="apple-touch-icon-precomposed" href="https://i1.wp.com/www.historicaengenharia.com/wp-content/uploads/2017/07/favicon.png?fit=128%2C128&#038;ssl=1" />
+<meta name="msapplication-TileImage" content="https://i1.wp.com/www.historicaengenharia.com/wp-content/uploads/2017/07/favicon.png?fit=128%2C128&#038;ssl=1" />
 	</head>
 	<body class="login login-action-login wp-core-ui  locale-en-us jetpack-sso jetpack-sso-form-display">
 		<div id="login">
 		<h1><a href="https://wordpress.org/" title="Powered by WordPress" tabindex="-1">Histórica Engenharia</a></h1>
 	
-<<<<<<< HEAD
-<form name="loginform" id="loginform" action="https://henas01f.myqnapcloud.com/wp-login.php" method="post">
-=======
-<form name="loginform" id="loginform" action="https://historicaeng.github.io/wp-login.php" method="post">
->>>>>>> parent of 406cb7f... update-2017-10-17
+<form name="loginform" id="loginform" action="https://www.historicaengenharia.com/wp-login.php" method="post">
 	<p>
 		<label for="user_login">Username or Email Address<br />
 		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
@@ -58,21 +44,11 @@
 		<label for="user_pass">Password<br />
 		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
 	</p>
-				<div style="margin: 5px 0 20px;">
-				<strong>Prove your humanity: </strong>
-				5 &nbsp; + &nbsp; 2 &nbsp; = &nbsp;
-				<input type="input" name="jetpack_protect_num" value="" size="2" />
-				<input type="hidden" name="jetpack_protect_answer" value="49e2fd3e7d93d22ecc165aebad2c2badf6ecc2ef" />
-			</div>
-				<div id="jetpack-sso-wrap">
+			<div id="jetpack-sso-wrap">
 			
 
 			<div id="jetpack-sso-wrap__action">
-<<<<<<< HEAD
-				<a rel="nofollow" href="https://henas01f.myqnapcloud.com/wp-login.php?action=jetpack-sso" class="jetpack-sso button button-primary"><span><span class="genericon genericon-wordpress"></span> Log in with WordPress.com</span></a>
-=======
-				<a rel="nofollow" href="https://historicaeng.github.io/wp-login.php?action=jetpack-sso" class="jetpack-sso button button-primary"><span><span class="genericon genericon-wordpress"></span> Log in with WordPress.com</span></a>
->>>>>>> parent of 406cb7f... update-2017-10-17
+				<a rel="nofollow" href="https://www.historicaengenharia.com/wp-login.php?action=jetpack-sso" class="jetpack-sso button button-primary"><span><span class="genericon genericon-wordpress"></span> Log in with WordPress.com</span></a>
 									<p>
 						You can now save time spent logging in by connecting your WordPress.com account to Histórica Engenharia.					</p>
 							</div>
@@ -81,37 +57,22 @@
 					<span>Or</span>
 				</div>
 
-<<<<<<< HEAD
-				<a href="http://www.historicaengenharia.com/wp-login.php?jetpack-sso-show-default-form=1" class="jetpack-sso-toggle wpcom">
+				<a href="https://www.historicaengenharia.com/wp-login.php?jetpack-sso-show-default-form=1" class="jetpack-sso-toggle wpcom">
 					Log in with username and password				</a>
 
-				<a href="http://www.historicaengenharia.com/wp-login.php?jetpack-sso-show-default-form=0" class="jetpack-sso-toggle default">
-=======
-				<a href="https://historicaeng.github.io/wp-login.php?jetpack-sso-show-default-form=1" class="jetpack-sso-toggle wpcom">
-					Log in with username and password				</a>
-
-				<a href="https://historicaeng.github.io/wp-login.php?jetpack-sso-show-default-form=0" class="jetpack-sso-toggle default">
->>>>>>> parent of 406cb7f... update-2017-10-17
+				<a href="https://www.historicaengenharia.com/wp-login.php?jetpack-sso-show-default-form=0" class="jetpack-sso-toggle default">
 					Log in with WordPress.com				</a>
 					</div>
 			<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Remember Me</label></p>
 	<p class="submit">
 		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
-<<<<<<< HEAD
-		<input type="hidden" name="redirect_to" value="https://henas01f.myqnapcloud.com/wp-admin/" />
-=======
-		<input type="hidden" name="redirect_to" value="https://historicaeng.github.io/wp-admin/" />
->>>>>>> parent of 406cb7f... update-2017-10-17
+		<input type="hidden" name="redirect_to" value="https://www.historicaengenharia.com/wp-admin/" />
 		<input type="hidden" name="testcookie" value="1" />
 	</p>
 </form>
 
 <p id="nav">
-<<<<<<< HEAD
-	<a href="https://henas01f.myqnapcloud.com/wp-login.php?action=lostpassword">Lost your password?</a>
-=======
-	<a href="https://historicaeng.github.io/wp-login.php?action=lostpassword">Lost your password?</a>
->>>>>>> parent of 406cb7f... update-2017-10-17
+	<a href="https://www.historicaengenharia.com/wp-login.php?action=lostpassword">Lost your password?</a>
 </p>
 
 <script type="text/javascript">
@@ -135,20 +96,12 @@ wp_attempt_focus();
 if(typeof wpOnload=='function')wpOnload();
 </script>
 
-<<<<<<< HEAD
-	<p id="backtoblog"><a href="http://www.historicaengenharia.com/">&larr; Back to Histórica Engenharia</a></p>
-=======
-	<p id="backtoblog"><a href="https://historicaeng.github.io/">&larr; Back to Histórica Engenharia</a></p>
->>>>>>> parent of 406cb7f... update-2017-10-17
+	<p id="backtoblog"><a href="https://www.historicaengenharia.com/">&larr; Back to Histórica Engenharia</a></p>
 	
 	</div>
 
 	
-<<<<<<< HEAD
-	<link rel='stylesheet' id='jetpack_css-css'  href='https://henas01f.myqnapcloud.com/wp-content/plugins/jetpack/css/jetpack.css?ver=5.4' type='text/css' media='all' />
-=======
-	<link rel='stylesheet' id='jetpack_css-css'  href='https://historicaeng.github.io/wp-content/plugins/jetpack/css/jetpack.css?ver=5.4' type='text/css' media='all' />
->>>>>>> parent of 406cb7f... update-2017-10-17
+	<link rel='stylesheet' id='jetpack_css-css'  href='https://www.historicaengenharia.com/wp-content/plugins/jetpack/css/jetpack.css?ver=5.4' type='text/css' media='all' />
 <script type="text/javascript">jQuery(function($) {
   
   var dfltlogo = $('.site-logo').attr('src');
